5b3d8f1b2627e0d97dbf3d48450bb1dbfbac3108
Tumelo <tumelophuti@gmail.com>
2017-06-29 10:21:53 +0200

    First Commit

---
+++ XamarinFInputValidation/XamarinFInputValidationPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinFInputValidation
+{
+    public partial class XamarinFInputValidationPage : ContentPage
+    {
+        public XamarinFInputValidationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
